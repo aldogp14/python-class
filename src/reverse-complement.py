9d9a8e4ab997e064f58dfdba521095749b47239b
Aldo <aldogp@lcg.unam.mx>
2022-03-03 12:03:47 -0600

    se agrego src

---
+++ src/reverse-complement.py
@@ -0,0 +1,27 @@
+'''
+
+NAME
+	reverse-complement.py
+    
+VERSION
+    1.0
+    
+AUTHOR
+	Aldo Garcia Prado :)
+  
+DESCRIPTION
+	Make the reverse complement of DNA sequence
+    
+CATEGORY
+	Genomic Sequence
+    
+USAGE
+
+    % python reverse-complement.py -i FILENAME
+    
+    example
+    
+    % python reverse-complement -i sequence.txt
+
+'''
+
