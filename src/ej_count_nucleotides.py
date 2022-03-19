dd793e7f3364748a3984267544b13f2675df276c
Aldo <aldogp@lcg.unam.mx>
2022-03-19 11:14:18 -0600

    se agrega documentacion a ej_count_nucleotides.py

--- src/ej_count_nucleotides.py
+++ src/ej_count_nucleotides.py
@@ -1,6 +1,7 @@
-
+# se pide la secuencia de DNA
 seq = input(F"Dame una secuencia de DNA:\n")
 
+# se imprimen las frecuencias
 print("frecuencia de A: ", seq.count('A'))
 
 print("frecuencia de T: ", seq.count('T'))
