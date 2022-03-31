9e4e62f6f3c96cb335986dd29b078b6f0dd80f4c
Aldo <aldogp@lcg.unam.mx>
2022-03-31 16:13:23 -0600

    mejoro la parte de imprimir a pantalla la cuenta, agrego encabezado

--- src/ej_count_nucleotides.py
+++ src/ej_count_nucleotides.py
@@ -1,11 +1,25 @@
-# se pide la secuencia de DNA
+'''
+NAME
+    ej_count_nucleotides
+
+VERSION
+   1.0
+
+AUTHOR
+    Aldo Garcia Prado
+
+DESCRIPTION
+    Programa que cuenta el numero de A, T, C y G en una secuencia introducida por el usuario.
+
+USAGE
+
+
+ARGUMENTS
+
+'''
+# Pedir la secuencia
 seq = input(F"Dame una secuencia de DNA:\n")
 
-# se imprimen las frecuencias
-print("frecuencia de A: ", seq.count('A'))
-
-print("frecuencia de T: ", seq.count('T'))
-
-print("frecuencia de C: ", seq.count('C'))
-
-print("frecuencia de G: ", seq.count('G'))
+# Imprimir las frecuencias contando cuantas veces aparecen A, T, C o G segun sea el caso.
+print(f"\nfrecuencia de A:", seq.count('A'), "\nfrecuencia de T:", seq.count(
+    'T'), "\nfrecuencia de C:", seq.count('C'), "\nfrecuencia de G:", seq.count('G'), "\n")
