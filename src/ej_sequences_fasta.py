83c4de35a92bebe5d87870701e644042d286a5b0
Eduardo Arrieta <61000997+eduardo-arrieta@users.noreply.github.com>
2022-04-21 04:21:49 -0500

    Cierre y extensión de archivos
    El código funciona. Recuerda cerrar siempre cualquier archivo que abras, tanto para los que lees como para los que escribes. Otra cosa sería que la variable `s` tiene un nombre algo abstracto, te recominedo usar palabras como `record` o `header` para no repetir secuencia. Por último, los archivos en formato fasta tienen su propia extensión `.fasta`, así tu output quedaría mejor.

--- src/ej_sequences_fasta.py
+++ src/ej_sequences_fasta.py
@@ -12,26 +12,27 @@ DESCRIPTION
     Programa que convierte un input con el nombre de la secuencia y la secuencia a un archivo con formato fasta.
 
 USAGE
-    null
+    py ej_sequences_fasta.py
 
 ARGUMENTS
     null
 '''
 # Guardar las secuencias como lista.
-file = open('data/dna_sequences.txt')
+file = open('data/dna_sequences.txt', 'r')
 sequences = file.readlines()
+file.close()
 
 # Abrir el archivo para el fasta
-fasta = open("results/sequences_fasta.txt", 'w')
+fasta = open("results/sequences_fasta.fasta", 'w')
 
 # Recorrer cada elemento de la lista.
 # Primero hacer modificaciones generales y luego especificas, como el upper y reemplazar caracteres.
 # Guardar cada elemento modificado en un archivo.
 for sequence in sequences:
-    seq = sequence.replace(F"{sequence}", F"> {sequence}").split("   ")
-    seq[1] = seq[1].upper().replace("-", "", 1000).rstrip('\n')
-    for s in seq:
-        fasta.write(F"{s}\n")
+    record = sequence.replace(sequence, f"> {sequence}").split("   ")
+    record[1] = record[1].upper().replace("-", "", 1000).rstrip('\n')
+    for element in record:
+        fasta.write(f"{element}\n")
 
 # Cerrar el archivo
 fasta.close()
