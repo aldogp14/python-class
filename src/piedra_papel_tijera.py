4a8db65a799810106ce0c710954310fcded04d6e
Eduardo Arrieta <61000997+eduardo-arrieta@users.noreply.github.com>
2022-04-28 00:38:17 -0500

    Cambios en presentacion
    Funciona tu código, pero hay algunos detalles. Lo primero que se observa es que no se respeta la regla del máximo número de caracteres por línea de código. Tienes lineas muy grandes que se ven mal. Tu código tampoco está separado por bloques. Tampoco pides el nombre del jugador. Podrías mejorar el código agregando una lista con los posibles valores que puede escoger la máquina y escoger uno con random.choice(), random.randint() si es una opción, pero si lo vas a usar para sacar índices pierde un poco sus sentido. Y ya solo como recomendación futura, sería un buen gesto decirle al usuario por qué perdió.

--- src/piedra_papel_tijera.py
+++ src/piedra_papel_tijera.py
@@ -9,10 +9,11 @@ AUTHOR
     Aldo Garcia Prado
 
 DESCRIPTION
-    Prgorama que simula piedra, papel o tijera contra la computadora. La eleccion de la computadora se hace de forma random.
+    Prgorama que simula piedra, papel o tijera contra la computadora. 
+    La eleccion de la computadora se hace de forma random.
 
 USAGE
-    null
+    py src/piedra_papel_tijera.py
 
 ARGUMENTS
     null
@@ -20,26 +21,25 @@ ARGUMENTS
 import random
 
 # Pedir la eleccion al usuario
+nombre_usuario = input("Introduzca su nombre:")
+
 print("Introduce tu eleccion: piedra, papel o tijera")
 user = input()
 
-# generar numero aleatorio del 1 al 3
-num = random.randint(1, 3)
-
 # Asignar una eleccion de piedra, papel o tijera conforme al numero aleatorio
-if num == 1:
-    cpu = "piedra"
-elif num == 2:
-    cpu = "papel"
-else:
-    cpu = "tijera"
+opciones = ["piedra", "papel", "tijera"]
+cpu = random.choice(opciones)
 
 # Cuando las elecciones son iguales, hay un empate.
 if user == cpu:
     print("Empate, vuelve a jugar.")
-# Verificar todas las opciones en las que ganas (usuario vs cpu): tijera vs papel || papel vs piedra || piedra vs tijera
+    
+# Verificar todas las opciones en las que ganas (usuario vs cpu): 
+# tijera vs papel || papel vs piedra || piedra vs tijera
+# Aqui podrias considerar dar enters o hacer otros elif, te lo deja a tu eleccion
 elif (user == "tijera" and cpu == "papel") or (user == "papel" and cpu == "piedra") or (user == "piedra" and cpu == "tijera"):
-    print("Ganaste")
+    print(nombre_usuario, "Ganaste contra", cpu)
+    
 # Si no hubo empate ni tampoco gano el usuario, entonces perdio.
 else:
-    print("Perdiste")
+    print(nombre_usuario, "Perdiste contra", cpu)
