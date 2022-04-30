10063b68c239a0990aa6041f5c451379c94e95bc
Aldo <aldogp@lcg.unam.mx>
2022-04-30 16:16:43 -0500

    Se le cambio el nombre a fasta.py y se le agregaron excepciones

---
+++ src/fasta.py
@@ -0,0 +1,39 @@
+'''
+NAME
+    ej_fasta.py
+
+VERSION
+   1.1
+
+AUTHOR
+    Aldo Garcia Prado
+
+DESCRIPTION
+    Programa que crea un archivo fasta a partir de una secuencia en cierta direccion establecida.
+
+USAGE
+    null
+
+ARGUMENTS
+    null
+'''
+from typing import IO
+
+try:
+    # Guardar la secuencia de interes en una variable
+    file_name = 'data/dna.txt'
+
+# ver si la direccion en el codigo es correcta
+except IOError as ex:
+    print(F"El archivo {ex.filename} no se encuentra\n")
+
+else:
+    file = open(file_name)
+    content = file.read().rstrip('\n')
+
+    # Abrir un archivo en carpeta data y escribir la secuencia con formato FASTA
+    fasta = open("results/dna.fasta", 'w')
+    fasta.write(F">sequence_name\n{content}")
+
+    # Cerrar el archivo
+    fasta.close()
