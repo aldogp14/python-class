dee97f2c13dab47684653f3021c9b1685e74d1cb
Aldo <aldogp@lcg.unam.mx>
2022-04-30 16:28:56 -0500

    Se agregan excepciones a ej_porcentaje_GCAT.py

--- src/ej_porcentaje_GCAT.py
+++ src/ej_porcentaje_GCAT.py
@@ -3,7 +3,7 @@ NAME
     ej_porcentaje_GCAT
 
 VERSION
-   1.0
+   1.1
 
 AUTHOR
     Aldo Garcia Prado
@@ -12,21 +12,35 @@ DESCRIPTION
     Programa que indica los porcentajes de GC y AT cuando le introduces la direccion de la secuencia.
 
 USAGE
-    null
+    usar el archivo en la direccion: data/dna.txt
 
 ARGUMENTS
     null
 '''
+from typing import IO
 
-# Pedir la secuencia al usuario y guardarla en una variable
+# Pedir la direccion al usuario y guardarla en una variable
 nombre_archivo = input("Introduce la direccion del archivo con la secuencia: ")
-archivo = open(nombre_archivo)
-contenido = archivo.read().rstrip('\n')
 
-# Hacer las cuentas y calculos para los porcentajes
-long = len(contenido)
-porcentaje_AT = ((contenido.count('A') + contenido.count('T')) / long * 100)
-porcentaje_GC = ((contenido.count('G') + contenido.count('C')) / long * 100)
+# Ver si la direccion es correcta
+try:
+    archivo = open(nombre_archivo)
+
+except IOError as ex:
+    print(F"El archivo {ex.filename} no se encuentra\n")
+
+else:
+    seq = archivo.read().rstrip('\n')
+
+    # Ver si la secuencia tiene Ns y mandar un error si asi es.
+    if seq.count('N') > 0:
+        raise ValueError(F"La secuencia contiene {seq.count('N')} Ns")
+
+    # Hacer las cuentas y calculos para los porcentajes
+    else:
+        long = len(seq)
+        porcentaje_AT = ((seq.count('A') + seq.count('T')) / long * 100)
+        porcentaje_GC = ((seq.count('G') + seq.count('C')) / long * 100)
 
 # imprimir los porcentajes
 print(
