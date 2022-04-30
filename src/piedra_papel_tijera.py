7e348ffe845dbdfa6b5e8f37f02be5683bbe1a88
Aldo <aldogp@lcg.unam.mx>
2022-04-30 16:49:46 -0500

    Se le agrega la opcion de poder seguir jugando

--- src/piedra_papel_tijera.py
+++ src/piedra_papel_tijera.py
@@ -19,27 +19,33 @@ ARGUMENTS
 '''
 import random
 
-# Pedir la eleccion al usuario
-print("Introduce tu eleccion: piedra, papel o tijera")
-user = input()
-
 # generar numero aleatorio del 1 al 3
 num = random.randint(1, 3)
+play = "si"
 
-# Asignar una eleccion de piedra, papel o tijera conforme al numero aleatorio
-if num == 1:
-    cpu = "piedra"
-elif num == 2:
-    cpu = "papel"
-else:
-    cpu = "tijera"
 
-# Cuando las elecciones son iguales, hay un empate.
-if user == cpu:
-    print("Empate, vuelve a jugar.")
-# Verificar todas las opciones en las que ganas (usuario vs cpu): tijera vs papel || papel vs piedra || piedra vs tijera
-elif (user == "tijera" and cpu == "papel") or (user == "papel" and cpu == "piedra") or (user == "piedra" and cpu == "tijera"):
-    print("Ganaste")
-# Si no hubo empate ni tampoco gano el usuario, entonces perdio.
-else:
-    print("Perdiste")
+while play.lower() == "si":
+    # Pedir la eleccion al usuario
+    print("Introduce tu eleccion: piedra, papel o tijera")
+    user = input()
+
+    # Asignar una eleccion de piedra, papel o tijera conforme al numero aleatorio
+    if num == 1:
+        cpu = "piedra"
+    elif num == 2:
+        cpu = "papel"
+    else:
+        cpu = "tijera"
+
+    # Cuando las elecciones son iguales, hay un empate.
+    if user.lower == cpu:
+        print("Empate")
+    # Verificar todas las opciones en las que ganas (usuario vs cpu): tijera vs papel || papel vs piedra || piedra vs tijera
+    elif (user.lower == "tijera" and cpu == "papel") or (user.lower == "papel" and cpu == "piedra") or (user.lower == "piedra" and cpu == "tijera"):
+        print("Ganaste")
+    # Si no hubo empate ni tampoco gano el usuario, entonces perdio.
+    else:
+        print("Perdiste")
+
+    print(F"¿Quieres jugar de nuevo?(si/no)")
+    play = input()
