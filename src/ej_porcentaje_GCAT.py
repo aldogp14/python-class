557974c906d157be2a74e27765cc9b10402ca675
Aldo <aldogp@lcg.unam.mx>
2022-05-11 21:36:36 -0500

    se añada objeto argparser a ej_porcentaje_GCAT.py

--- src/ej_porcentaje_GCAT.py
+++ src/ej_porcentaje_GCAT.py
@@ -19,9 +19,21 @@ ARGUMENTS
 '''
 from multiprocessing.sharedctypes import Value
 from typing import IO
+import argparse
 
-# Pedir la direccion al usuario y guardarla en una variable
-nombre_archivo = input("Introduce la direccion del archivo con la secuencia: ")
+# crear parser
+parser = argparse.ArgumentParser(
+    description="Script que calcula el contenido de AT usando argumentos por linea de comando")
+# añadir argumentos
+parser.add_argument("-i", "--input", metavar="path/to/file",
+                    help="File with gene sequences", required=True)
+parser.add_argument(
+    "-o", "--output", help="Direccion del archivo de salida", required=False)
+# ejecutar
+args = parser.parse_args()
+# guardar en variables
+nombre_archivo = args.input
+nombre_output = args.output
 
 # Ver si la direccion es correcta
 try:
@@ -56,6 +68,16 @@ else:
             porcentaje_AT = ((seq.count('A') + seq.count('T')) / long * 100)
             porcentaje_GC = ((seq.count('G') + seq.count('C')) / long * 100)
 
-# imprimir los porcentajes
-print(
-    F"Porcentaje de AT: {porcentaje_AT}%\nPorcentaje de GC: {porcentaje_GC}%\n")
+# condicion para ver si nombre_output fue rellenada
+if nombre_output:
+    # mandar el resultado a la direccion introducida por el usuario
+    file = nombre_output
+    file = open(nombre_output, 'w')
+    file.write(
+        F"Porcentaje de AT: {porcentaje_AT}%\nPorcentaje de GC: {porcentaje_GC}%")
+    file.close()
+    print(F"Los porcentajes se encuentran en: {nombre_output}")
+else:
+    # imprimir los porcentajes
+    print(
+        F"Porcentaje de AT: {porcentaje_AT}%\nPorcentaje de GC: {porcentaje_GC}%\n")
